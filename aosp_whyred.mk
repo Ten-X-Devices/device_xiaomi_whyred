542d8a8b3aa12c9c8280d98870889734559e0c3d
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-03-07 20:13:06 +0530

    whyred : Ten-X~fy!

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -19,15 +19,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Ten-X OS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# PixelExperience Properties
+# Tenx Properties
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := true
+CUSTOM_BUILD_TYPE := Official
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-pe
+    $(DEVICE_PATH)/overlay-tenx
 
 # Powerhint
 EAS_POWERHINT_VARIANT := sdm636
